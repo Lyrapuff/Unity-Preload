204bf681453f8e665771eed791be39cb3f2e1d7c
Lyrapuff <lyrapuff@gmail.com>
2021-03-26 23:33:47 +0300

    1.0.0 package

---
+++ Runtime/PreloadedAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmallTail.Preload
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PreloadedAttribute : Attribute
+    {
+        public string Name;
+
+        public PreloadedAttribute()
+        {
+            
+        }
+
+        public PreloadedAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Runtime/Preloader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace SmallTail.Preload
+{
+    public static class Preloader
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void OnLoad()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            int count = 0;
+            
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (Assembly assembly in assemblies)
+            {
+                IEnumerable<Type> preloads = assembly.GetTypes()
+                    .Where(x => x.GetCustomAttribute(typeof(PreloadedAttribute)) != null);
+
+                foreach (Type preload in preloads)
+                {
+                    if (preload.BaseType == typeof(MonoBehaviour))
+                    {
+                        GameObject instance = new GameObject();
+
+                        string name = (preload.GetCustomAttribute(typeof(PreloadedAttribute)) as PreloadedAttribute)?.Name ?? preload.Name;
+                        instance.name = name;
+                        
+                        instance.AddComponent(preload);
+                        Object.DontDestroyOnLoad(instance);
+
+                        count++;
+                    }
+                }
+            }
+            
+            sw.Stop();
+            Debug.Log($"[ST Preloader] Preloaded {count} {(count == 1 ? "script" : "scripts")} in {sw.Elapsed.TotalSeconds}s.");
+        }
+    }
+}
